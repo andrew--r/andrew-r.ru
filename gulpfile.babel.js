646d6c755aee4fc169746b7ef1a894fe973c33e5
Andrew Romanov <me@andrew-r.ru>
2017-08-26 14:32:46 +0300

    v2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,35 +1,30 @@
 import gulp from 'gulp';
 
 import postcss from 'gulp-postcss';
-import atImport from 'postcss-import';
-import cssnext from 'cssnext';
+import postcssAtImport from 'postcss-import';
+import postcssCalc from 'postcss-calc';
+import postcssCustomProperties from 'postcss-custom-properties';
 import csso from 'gulp-csso';
-
 import uncache from 'gulp-uncache';
 import minifyMarkup from 'gulp-htmlmin';
-
 import ftp from 'vinyl-ftp';
 import gutil from 'gulp-util';
 import ftpCredentials from './ftp.json';
-
 
 const SOURCE = './source';
 const OUT = './build';
 const STATIC_OUT = `${OUT}/static`;
 const FTP_DEST = '/httpdocs';
 
-gulp.task('styles', () => {
-	const postcssPlugins = [
-		atImport(),
-		cssnext(),
-	];
-
-	return gulp
-		.src(`${SOURCE}/styles/main.css`)
-		.pipe(postcss(postcssPlugins))
-		.pipe(csso())
-		.pipe(gulp.dest(STATIC_OUT))
-})
+gulp.task('styles', () =>  gulp
+	.src(`${SOURCE}/styles/design-system.css`)
+	.pipe(postcss([
+		postcssAtImport(),
+		postcssCalc(),
+	]))
+	.pipe(csso())
+	.pipe(gulp.dest(STATIC_OUT))
+);
 
 gulp.task('markup', ['styles'], () => {
 	return gulp
