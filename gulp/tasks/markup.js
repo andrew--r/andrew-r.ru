f708b2b7c95c40d749179b28933770029c60bf87
Andrew Romanov <me@andrew-r.ru>
2017-08-28 00:43:09 +0300

    Use pug templates instead of plain html

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,11 +1,13 @@
 import gulp from 'gulp';
+import pug from 'gulp-pug';
 import uncache from 'gulp-uncache';
 import minifyMarkup from 'gulp-htmlmin';
 import { SOURCE, OUT } from '../config';
 
 gulp.task('markup', ['styles'], () => {
   return gulp
-    .src(`${SOURCE}/index.html`)
+    .src(`${SOURCE}/pages/**/*.pug`)
+    .pipe(pug())
     .pipe(uncache())
     .pipe(minifyMarkup({
       collapseWhitespace: true,
